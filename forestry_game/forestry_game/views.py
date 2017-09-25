c8d0e7ce7a92a50d394481216d983c24ce99b6a6
Oiva Moisio <ancientrib@gmail.com>
2017-09-25 19:50:37 +0300

    Some small bug fixes (and initial commit before model tests).

--- forestry_game/forestry_game/views.py
+++ forestry_game/forestry_game/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from forestry_game.models import CustomUser, Map, Execution
-
 def home ( request ):
     context = dict()
     return render(request, 'index.html', context)
