08dc873406204315ff8bbad8bfced4b9e3e1e1da
Oiva Moisio <ancientrib@gmail.com>
2017-11-12 18:04:31 +0200

    Some more database settings

--- forestry_game/deploy_settings.py
+++ forestry_game/deploy_settings.py
@@ -102,9 +102,28 @@ if (USE_LOCAL_DATABASE):
         }
     }
 else:
+    import os
+    from urllib import parse
+    import psycopg2
+
+    parse.uses_netloc.append("postgres")
+    url = parse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
     # Update database configuration with $DATABASE_URL.
     import dj_database_url
     db_from_env = dj_database_url.config()
+    DATABASES = {
+        'default': {
+        }
+    }
     DATABASES['default'].update(db_from_env)
 
 
