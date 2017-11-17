508e4f381b250b9911d1767365e703868722d92b
Oiva Moisio <ancientrib@gmail.com>
2017-11-17 10:26:42 +0200

    Reordered static files.

--- forestry_game/deploy_settings.py
+++ forestry_game/deploy_settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '=*$j%1gna+opauu!7qbk5p@p%p8wlqw#$ypp%tmp(!x#aj5t8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-USE_LOCAL_DATABASE = False
+USE_LOCAL_DATABASE = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'forestry-game-backend.herokuapp.com', 'forestry-game.herokuapp.com']
 
@@ -155,9 +155,10 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static", "static_root")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static_root")
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
--- forestry_game/settings.py
+++ forestry_game/settings.py
@@ -140,17 +140,18 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = BASE_DIR
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static", "static_root")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static_root")
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(PROJECT_ROOT, 'static'),
 )
